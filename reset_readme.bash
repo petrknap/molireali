b8281610650be1725e30a6cee71fc97afdfa5dc3
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-07-05 13:21:05 +0200

    feat: Implemented reset_readme plugin

---
+++ reset_readme.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function reset_readme {
+  function remove_tail() {
+    README_MD_CONTENT="$(cat "${README_MD_FILE}")"
+    README_MD_CONTENT="${README_MD_CONTENT%---*}"
+    echo "${README_MD_CONTENT}" > "${README_MD_FILE}"
+  }
+
+  remove_tail # ---
+  remove_tail # \n
+
+  echo "" >> "${README_MD_FILE}"
+  echo "---" >> "${README_MD_FILE}"
+  echo "" >> "${README_MD_FILE}"
+}
--- run.bash
+++ run.bash
@@ -3,11 +3,14 @@ set -e
 
 DIR="$(realpath ${BASH_SOURCE%/*})"
 RUN_FILE=".$(basename ${DIR})"
+README_MD_FILE="README.md"
 
-# Include modules
+# Include plugins
+. ${DIR}/reset_readme.bash
 
 # Run scripts
 for RUN_FILE_PATH in ${DIR}/../*/${RUN_FILE}; do (
   cd $(dirname ${RUN_FILE_PATH})
+  reset_readme
   . ${RUN_FILE}
 ); done

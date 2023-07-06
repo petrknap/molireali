5baa6e1c6f4fc0934af52c9980d2f52b7907a645
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-07-06 09:29:19 +0200

    feat: added MIT license to license plugin

--- license.bash
+++ license.bash
@@ -2,13 +2,18 @@
 
 function license {
   LICENSE="${1}"
+  YEAR="${2}"
+  AUTHOR="${3}"
 
   for LICENSE_FILE in "${DIR}/license/${LICENSE}/"*; do (
     LICENSE_FILE_TARGET="$(basename "${LICENSE_FILE}")"
+    LICENSE_FILE_CONTENT="$(cat "${LICENSE_FILE}")"
+    LICENSE_FILE_CONTENT="${LICENSE_FILE_CONTENT/\$YEAR/$YEAR}"
+    LICENSE_FILE_CONTENT="${LICENSE_FILE_CONTENT/\$AUTHOR/$AUTHOR}"
     if [[ "${LICENSE_FILE_TARGET}" == "${README_MD_FILE}" ]]; then
-      echo "$(cat "${LICENSE_FILE}")" >> "${LICENSE_FILE_TARGET}"
+      echo "${LICENSE_FILE_CONTENT}" >> "${LICENSE_FILE_TARGET}"
     else
-      echo "$(cat "${LICENSE_FILE}")" > "${LICENSE_FILE_TARGET}"
+      echo "${LICENSE_FILE_CONTENT}" > "${LICENSE_FILE_TARGET}"
     fi
   ); done
 }

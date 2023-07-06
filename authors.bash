ca445b286247af1ca3c98a1e129dc7ebd7fdf171
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-07-06 13:14:05 +0200

    feat: authors plugin now sorts AUTHORS by count of commits

--- authors.bash
+++ authors.bash
@@ -3,5 +3,5 @@
 function authors {
   echo "# This is the official list of authors for copyright purposes." > "AUTHORS"
   echo "" >> "AUTHORS"
-  git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf >> "AUTHORS"
+  git shortlog --summary --email --numbered --all --no-merges | cut -f2- >> "AUTHORS"
 }

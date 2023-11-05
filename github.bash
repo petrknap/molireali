d74a681786186d2345858e33cdcbe0d9d2eafda1
Petr Knap <8299754+petrknap@users.noreply.github.com>
2023-11-05 18:13:28 +0000

    feat: improved github docker workflow

--- github.bash
+++ github.bash
@@ -7,6 +7,6 @@ function github-workflow {
 
   mkdir --parent "${WORKFLOW_DIRECTORY}" || true
   WORKFLOW_CONTENT="$(cat "${DIR}/${WORKFLOW_DIRECTORY#?}/${WORKFLOW}.yaml")"
-  WORKFLOW_CONTENT="${WORKFLOW_CONTENT/\$1/$WORKFLOW_ARG1}"
+  WORKFLOW_CONTENT="$(echo "${WORKFLOW_CONTENT}" | sed "s/\$1/${WORKFLOW_ARG1}/g")"
   echo "${WORKFLOW_CONTENT}" > "${WORKFLOW_DIRECTORY}/${WORKFLOW}.yaml"
 }
